b5de6fc276e530a548b93286bb69b8e4f934c04f
Gouri Saini <gourisaini@Gouris-MacBook-Air.local>
2025-10-07 00:00:30 +0530

    deploy

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 require("./models/db");
-const bodyParser = require("body-parser")
-const indexRouter = require("./routes/index")
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 const Port = process.env.PORT;
 
-app.listen(Port, () => console.log(`Server is running successfully on Port ${Port}`));
+app.listen(Port, () =>
+  console.log(`Server is running successfully on Port ${Port}`)
+);
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://localhost:3000', 
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND,
+  })
+);
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
